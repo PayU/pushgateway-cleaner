6d620a935a896f7e0b2ce133567154a9837a800c
GoniGo <goni.gonen@payu.com>
2020-11-19 21:38:00 +0200

    try again with script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -e
+# set -e
 
-version=$(cat package.json \
-| grep version \
-| head -1 \
-| awk -F: '{ print $2 }' \
-| sed 's/[",]//g')
+# version=$(cat package.json \
+# | grep version \
+# | head -1 \
+# | awk -F: '{ print $2 }' \
+# | sed 's/[",]//g')
 
-echo "::set-output name=version::$version"
+# echo "::set-output name=version::$version"
 
 exec npm run start
